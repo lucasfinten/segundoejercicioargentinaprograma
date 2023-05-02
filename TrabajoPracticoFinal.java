b3b7c319a49a7939a756d7410774a30d6f6d5def
lucasfinten <lucasfinten@gmail.com>
2023-05-01 23:37:50 -0300

    tp final argprograma

---
+++ TrabajoPracticoFinal.java
@@ -0,0 +1,15 @@
+package com.mycompany.trabajopracticofinal;
+
+import static com.mycompany.trabajopracticofinal.Principal.principal;
+import java.io.IOException;
+import java.sql.SQLException;
+
+
+public class TrabajoPracticoFinal {
+
+    public static void main(String[] args) throws SQLException, IOException {
+       
+        principal();
+
+    }
+}
